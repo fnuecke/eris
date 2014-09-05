32e390077ed7128730aac62daf875418947334e5
Florian Nücke <florian@nuecke.de>
2014-09-05 17:54:22 +0200

    Bump version for Lua patch.

--- src/eris.h
+++ src/eris.h
@@ -29,7 +29,7 @@ THE SOFTWARE.
 #define ERIS_VERSION_MAJOR  "1"
 #define ERIS_VERSION_MINOR  "0"
 #define ERIS_VERSION_NUM    100
-#define ERIS_VERSION_RELEASE  "0"
+#define ERIS_VERSION_RELEASE  "1"
 
 /*
 ** ==================================================================
